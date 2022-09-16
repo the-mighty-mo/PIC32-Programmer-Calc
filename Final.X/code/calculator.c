6e2fd09b2c6859f412388d3fab8fe8065fdcf97a
Benjamin Hall <hallb1016@gmail.com>
2022-09-15 21:51:55 -0400

    Started cleanup of Calculator module

--- Final.X/code/calculator.c
+++ Final.X/code/calculator.c
@@ -7,9 +7,11 @@
 #include "utils.h"
 #include <string.h>
 
+// Operands
 static uint16_t nums[2];
 static uint8_t num_idx;
 
+// Numerical base
 static enum NumBase {
 	Bin,
 	Dec,
@@ -17,6 +19,7 @@ static enum NumBase {
 } num_base;
 static uint8_t const bases[] = {2, 10, 16};
 
+// Operator
 static enum Operator {
 	Add,
 	Sub,
@@ -27,9 +30,12 @@ static enum Operator {
 	Xor
 } operator;
 
-static char lcd[2][17];
+// LCD output
+static char lcd[2][17] = {0};
+static uint8_t update_lcd[2] = {0};
 static char const operators[] = {'+', '-', '*', '/', '&', '|', '^'};
 
+// TODO: clean this up
 static void UpdateBase(void);
 static void NumToStr(uint16_t num, char *str, size_t strlen);
 static void UpdateLcd(void);
@@ -37,6 +43,7 @@ static void UpdateNum(uint8_t key);
 
 static void RunOp(void);
 
+/** Resets the operands and operator. */
 static void ResetNums(void)
 {
 	memset(nums, 0, sizeof(nums));
@@ -44,30 +51,36 @@ static void ResetNums(void)
 	RGBLED_SetValue(0, 0, 0);
 }
 
+/** Clears the LCD output. */
 static void ClearLcd(void)
 {
-	memset(lcd[0], ' ', sizeof(lcd[0]) - 1);
-	lcd[0][sizeof(lcd[0]) - 1] = '\0';
-	memset(lcd[1], ' ', sizeof(lcd[1]) - 1);
-	lcd[1][sizeof(lcd[1]) - 1] = '\0';
-
+	// First line: first operand
 	NumToStr(nums[0], lcd[0] + 1, sizeof(lcd[0]) - 2);
+	lcd[0][0] = ' ';
+
+	// Second line: operator and spaces
+	memset(lcd[1], ' ', sizeof(lcd[1]) - 1);
 	lcd[1][0] = operators[operator];
 
-	LCD_WriteStringAtPos(lcd[0], 0, 0);
-	LCD_WriteStringAtPos(lcd[1], 1, 0);
+	// Signal that we need to write to the LCD
+	update_lcd[0] = 1;
+	update_lcd[1] = 1;
 }
 
 void Calculator_Init(void)
 {
+	// Reset the operands and operator
 	ResetNums();
 	num_base = Hex;
 	operator = Add;
-	
+	// Clear the LCD display
 	ClearLcd();
 }
 
-void Calculator_Process(void)
+/**
+ * Reads the switches and updates the current operator.
+ */
+static void ProcessOperator(void)
 {
 	uint8_t const swt = Input_GetSwtGroup();
 	if (Input_IsNewSwtGroup() && (swt & (swt - 1)) == 0) {
@@ -76,13 +89,19 @@ void Calculator_Process(void)
 			if (swt & (1 << i)) {
 				operator = i;
 				lcd[1][0] = operators[operator];
-				LCD_WriteStringAtPos(lcd[1], 1, 0);
+				update_lcd[1] = 1;
 				break;
 			}
 		}
 	}
 	LED_SetGroupValue(1 << operator);
+}
 
+/**
+ * Reads the buttons and updates the numerical base.
+ */
+static void ProcessNumBase(void)
+{
 	if (Input_GetNewBtn(BTN_U_BIT)) {
 		if (++num_base > Hex) {
 			num_base = Bin;
@@ -94,7 +113,13 @@ void Calculator_Process(void)
 		}
 		UpdateBase();
 	}
-	
+}
+
+/**
+ * Reads the buttons and clears or deletes characters from the input.
+ */
+static void ProcessClearBackspace(void)
+{
 	if (Input_GetNewBtn(BTN_R_BIT)) {
 		if (nums[num_idx] == 0) {
 			ResetNums();
@@ -107,18 +132,39 @@ void Calculator_Process(void)
 		nums[num_idx] /= bases[num_base];
 		UpdateLcd();
 	}
-	
+}
+
+void Calculator_Process(void)
+{
+	// Process changes to the operator, numerical base, or clear/backspace
+	ProcessOperator();
+	ProcessNumBase();
+	ProcessClearBackspace();
+
+	// Process new input
 	if (Input_GetNewBtn(BTN_C_BIT)) {
+		// User submitted an operand
 		if (num_idx == 0) {
+			// User submitted first operand, switch to second and update LCD
 			++num_idx;
 			UpdateLcd();
 		} else {
+			// User submitted second operand, run the operation
 			RunOp();
 		}
 	} else if (Input_IsNewKey()) {
+		// User submitted another digit
 		int8_t const key = Input_GetKey();
 		if (key >= 0) {
+			// Valid key, update the current operand
 			UpdateNum(key);
+		}
+	}
+
+	// Update the LCD output
+	for (int i = 0; i < sizeof(update_lcd) / sizeof(*update_lcd); ++i) {
+		if (update_lcd[i]) {
+			LCD_WriteStringAtPos(lcd[i], i, 0);
 		}
 	}
 }
@@ -157,18 +203,21 @@ static void UpdateNum(uint8_t key)
 static void UpdateLcd(void)
 {
 	NumToStr(nums[num_idx], lcd[num_idx] + 1, sizeof(lcd[num_idx]) - 2);
-	LCD_WriteStringAtPos(lcd[num_idx], num_idx, 0);
+	update_lcd[num_idx] = 1;
+//	LCD_WriteStringAtPos(lcd[num_idx], num_idx, 0);
 	RGBLED_SetValue(0, 0, 0);
 }
 
 static void UpdateBase(void)
 {
 	NumToStr(nums[0], lcd[0] + 1, sizeof(lcd[0]) - 2);
-	LCD_WriteStringAtPos(lcd[0], 0, 0);
+	update_lcd[0] = 1;
+//	LCD_WriteStringAtPos(lcd[0], 0, 0);
 	
 	if (num_idx) {
 		NumToStr(nums[1], lcd[1] + 1, sizeof(lcd[1]) - 2);
-		LCD_WriteStringAtPos(lcd[1], 1, 0);
+		update_lcd[1] = 1;
+//		LCD_WriteStringAtPos(lcd[1], 1, 0);
 	}
 	
 	if (num_base == Bin && ((nums[0] & 0x8000) || (nums[1] & 0x8000))) {
@@ -178,6 +227,9 @@ static void UpdateBase(void)
 	}
 }
 
+/**
+ * Runs the arithmetic operation on the inputs.
+ */
 static void RunOp(void)
 {
 	uint32_t num = 0;
--- Final.X/code/calculator.h
+++ Final.X/code/calculator.h
@@ -1,4 +1,11 @@
 #pragma once
 
+/**
+ * Initializes the calculator module.
+ * Depends on the input module.
+ */
 void Calculator_Init(void);
+/**
+ * Processes the calculator module.
+ */
 void Calculator_Process(void);
